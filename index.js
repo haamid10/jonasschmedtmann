25bf3952b1aab47465b55d5e43a937ea1dcef1b1
haamid10 <apdixamiid919@gmail.com>
2023-03-30 07:14:44 +0300

    data fetched

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http')
 const url = require('url')
+const fs = require('fs')
 
 
 const server = http.createServer((req,res)=>{
@@ -10,6 +11,15 @@ const server = http.createServer((req,res)=>{
     res.end('This is the Overview');
   }else if(pathName === '/product') {
     res.end('product page')
+  }else if (pathName === '/api'){
+    fs.readFile(`${__dirname}/devData/data.json`,'utf-8',(err,data)=>{
+      const productData = JSON.parse(data)
+      console.log(productData)
+      res.writeHead(200, {'Content-type': 'application/json'})
+      res.end(data)
+    })
+
+    
   } else{
     res.writeHead(404,{
       'Content-type': 'text/html',
