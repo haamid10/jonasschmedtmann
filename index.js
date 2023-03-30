eead1fa382f09df4578ecec97c0c5453f52bcf02
haamid10 <apdixamiid919@gmail.com>
2023-03-30 07:01:28 +0300

    404

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ const url = require('url')
 
 const server = http.createServer((req,res)=>{
   console.log(req.url);
-  res.end('Hello from the server!')
+  const pathName = req.url;
+
+  if(pathName === '/' || pathName === '/overview'){
+    res.end('This is the Overview');
+  }else if(pathName === '/product') {
+    res.end('product page')
+  } else{
+    res.writeHead(404,{
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
+    })
+    res.end('<h1>Page not Found!</h1>')
+  }
 })
 
 server.listen(8000,()=> console.log('Listening to req on port 800'))
